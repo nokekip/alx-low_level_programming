a443afb44a85ce49ab4a9c0b2498af80acf8f861
nokekip <kipnoke@gmail.com>
2022-07-01 01:51:44 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints out last part of a quote in a standard error.
+ * Return 1 if success.
+ */
+	int main (void)
+{
+	write("2, and the piece of art is useful - Dora Korpar, 2015-10-19\n", )
+}
+
