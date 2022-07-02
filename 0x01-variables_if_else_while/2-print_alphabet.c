2e5d0923d41e2187987b584e5c7fc2f7feb5d6b9
nokekip <kipnoke@gmail.com>
2022-07-01 17:55:17 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cytpe.h>
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Return: Always 0 
+ */
+	int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
