5d9a2f1998407c85ff354f02006695709a2768c4
nokekip <kipnoke@gmail.com>
2022-07-05 19:20:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include "main.h"
 
 /**
@@ -11,11 +10,14 @@
 
 int main(void)
 {
-	const char str[] = "_putchar\n";
-	int len = strlen(str);
-	int i;
+	char text[9] = "_putchar";
+	int i = 0;
 
-	for (i = 0; i < len; i++)
-		_putchar(str[i]);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
