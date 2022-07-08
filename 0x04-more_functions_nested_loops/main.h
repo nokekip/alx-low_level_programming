520831bf0ff840bf97199fefb856fe5e7c08ed03
nokekip <kipnoke@gmail.com>
2022-07-08 05:41:13 -0700

    task eight

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -48,5 +48,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
+void print_square(int size);
 
 #endif /* main.h */
