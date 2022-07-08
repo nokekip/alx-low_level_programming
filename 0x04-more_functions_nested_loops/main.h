915035df464febe22853013ffbbe5bb4d1255c76
nokekip <kipnoke@gmail.com>
2022-07-08 02:27:26 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,29 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * _isupper - Check for uppercase characters
+ *
+ * Return: 1 (Success) 0 (Otherwise)
+ */
+
+int _isupper(int c);
+
+/**
+ * _isdigit - Check for uppercase characters
+ * @c: integer
+ * Return: 1 (Success) 0 (Otherwise)
+ */
+
+#endif /* main.h */
