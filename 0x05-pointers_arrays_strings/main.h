a10aeb71ccad24731633c615131b169890da92a1
nokekip <kipnoke@gmail.com>
2022-07-11 13:01:00 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+fndef _MAIN_H
+#define _MAIN_H
+
+#include <string.h>
+
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - reset the value of a pointer to 98
+ * @n: integer pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n);
+
+#endif
