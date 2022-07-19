fadeed3492d3ab26026799840ba5791d6c7a75aa
nokekip <kipnoke@gmail.com>
2022-07-19 12:41:27 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char);
+
+/**
+ * _memset - append n bytes of memory with a constant value
+ * @s: pointer to memory area
+ * @b: constant value
+ * @n: number of bytes to fill
+ * Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
