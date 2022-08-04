2e80130f3a4b247cff32fa69587a1481a889abae
nokekip <kipnoke@gmail.com>
2022-08-04 13:52:12 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,12 +17,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int index = 0;
 
 	if (array == NULL || cmp == NULL)
-	return (-1);
+		return (-1);
 
 	for (; index < size; index++)
 {
 	if (cmp(array[index]) != 0)
-	return (index);
+		return (index);
 }
 
 	return (-1);
