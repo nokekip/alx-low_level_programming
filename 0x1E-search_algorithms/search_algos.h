d166be02da98f00a6a4d705353e1f152a9ded967
nokekip <kipnoke@gmail.com>
2023-12-12 22:38:49 +0300

    Searches for a value in an array using linear search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGORITHMS
+#define SEARCH_ALGORITHMS
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Function Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif
